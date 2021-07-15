12f3063eab775fd7ccc7a823434545e7d0dd003b
dotun <you@example.com>
2021-07-15 20:48:32 +0100

    search, pagination frontend done

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -14,22 +14,22 @@ exports.newProduct =  catchAsyncErrors(async (req, res, next) => {
         success: true,
         product
     })
-
 })
 
 // Get all products   =>   /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
-    const resPerPage = 12;
-    const productCount = await Product.countDocuments();
+    const resPerPage = 2;
+    const productsCount = await Product.countDocuments();
     const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter().pagination(resPerPage)
     const products = await apiFeatures.query;
 
     res.status(200).json({
         success: true,
         count: products.length,
-        productCount,
-        products
+        productsCount,
+        products,
+        resPerPage,
     })
 })
 
